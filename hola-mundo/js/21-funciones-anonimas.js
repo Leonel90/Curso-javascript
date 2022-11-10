6a5826fded1d3d543694d17502e992eae185d3b5
Leonel90 <97122219+Leonel90@users.noreply.github.com>
2022-11-10 16:16:33 -0500

    Funciones Texto

--- hola-mundo/js/21-funciones-anonimas.js
+++ hola-mundo/js/21-funciones-anonimas.js
@@ -2,7 +2,47 @@
 
 //Funciones anónimas
 //Es una función que no tiene nombre
+//la funciones anónimas son una funcion dentro de otra o simplemente un colback
 
-var pelicula = function(nombre){
+/*var pelicula = function(nombre){
     return "La pelicula es: " + nombre;
+}*/
+
+function sumame(numero1, numero2, sumaYmustra, sumaporDos){
+    var sumar = numero1 + numero2;
+
+    //Invocacion de las funciones de callbacks
+    sumaYmustra(sumar);
+    sumaporDos(sumar);
+
+    return sumar;
 }
+
+//funcion de callbacks
+sumame(5, 7, function(dato){
+    console.log("La suma es: " + dato);
+},
+function(dato){
+    console.log("La suma por dos es: " + (dato*2));
+}
+)
+
+//Funciones flecha
+function sumame(numero1, numero2, sumaYmustra, sumaporDos){
+    var sumar = numero1 + numero2;
+
+    //Invocacion de las funciones de callbacks
+    sumaYmustra(sumar);
+    sumaporDos(sumar);
+
+    return sumar;
+}
+ 
+//funcion de callbacks
+sumame(5, 7, dato => {
+    console.log("La suma es: " + dato);
+},
+dato => {
+    console.log("La suma por dos es: " + (dato*2));
+}
+)
---
+++ hola-mundo/js/23-funciones-texto.js
@@ -0,0 +1,8 @@
+'use strict'
+
+//Transformacion de textos
+var numero = 444;
+var texto1 = "Bienvenido al curso de JavaScript";
+var texto2 = "Es muy buen curso";
+
+numero.toString();

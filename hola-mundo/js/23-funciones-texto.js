0ed2aee87e98fec1b0883bdf1b9442b81150d296
Leonel90 <97122219+Leonel90@users.noreply.github.com>
2022-11-11 16:22:42 -0500

    Funciones texto 2

--- hola-mundo/js/23-funciones-texto.js
+++ hola-mundo/js/23-funciones-texto.js
@@ -5,4 +5,5 @@ var numero = 444;
 var texto1 = "Bienvenido al curso de JavaScript";
 var texto2 = "Es muy buen curso";
 
-numero.toString();
+var dato = numero.toString();
+console.log(typeof dato);
